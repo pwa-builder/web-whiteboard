3f20b5aba349511f6c85acfb38a063fb03d1a789
justin willis <jgw9617@gmail.com>
2019-11-27 23:41:38 +0000

    chore(): work on new login flow

--- src/components/login-pop/login-pop.tsx
+++ src/components/login-pop/login-pop.tsx
@@ -1,17 +1,25 @@
-import { Component, State, h } from '@stencil/core';
+import { Component, Element, State, h } from '@stencil/core';
 
 
 @Component({
   tag: 'login-pop',
-  styleUrl: 'login-pop.css'
+  styleUrl: 'login-pop.css',
+  shadow: false
 })
 export class LoginPop {
 
   @State() loaded: boolean = false;
+  @State() loggedIn: boolean = false;
+
+  @Element() el: HTMLElement;
 
   async componentDidLoad() {
     await import('../../mgt.js');
     this.loaded = true;
+
+    this.el.querySelector('mgt-login').addEventListener('loginCompleted', () => {
+      this.loggedIn = true;
+    });
   }
 
   render() {
@@ -22,9 +30,13 @@ export class LoginPop {
         </div>
 
         <div id="msalBlock">
-          {this.loaded ? <div>
+          {this.loaded && this.loggedIn === false ? <div>
             <mgt-msal-provider scopes="Notes.Create UserActivity.ReadWrite.CreatedByApp Device.Read Device.Command" client-id="ea8ee476-a5c2-4617-b376-a3fb40e46864"></mgt-msal-provider>
             <mgt-login></mgt-login>
+          </div> : null}
+
+          {this.loaded && this.loggedIn === true ? <div id="signedInDiv">
+            <p>Signed In</p>
           </div> : null}
         </div>
       </div>
